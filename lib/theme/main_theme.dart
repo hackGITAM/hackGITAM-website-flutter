853ee76079a35da2c4732c5048e27a64748cbb77
Sanyam Sharma <sanyamsh20@gmail.com>
2022-10-05 02:09:32 +0530

    Defined the base theme

---
+++ lib/theme/main_theme.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class MainTheme {
+  static ThemeData get darkTheme {
+    return ThemeData(
+        backgroundColor: const Color(0xFF2E3440),
+        //Top gradient color for Heading 1
+        indicatorColor: const Color(0xFFCECECE),
+        //Top gradient color for Heading 2
+        shadowColor: const Color(0xFFB48EAD),
+        textTheme: const TextTheme(
+          headline1: TextStyle(
+            fontFamily: "HandelGothic",
+            fontSize: 60,
+          ),
+          headline2: TextStyle(
+            fontFamily: "HandelGothic",
+            fontSize: 60,
+          ),
+          bodyText1: TextStyle(
+            fontFamily: "HandelGothic",
+            fontSize: 27,
+            color: Color(0xFFCECECE),
+          ),
+
+          // button: TextStyle(
+          //   fontFamily: "HandelGothic",
+          //   fontSize: 30,
+          //   color: Color(0xFF2E3440),
+          // ),
+        ),
+        textButtonTheme: TextButtonThemeData(
+            style: TextButton.styleFrom(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+          foregroundColor: const Color(0xFF2E3440),
+          backgroundColor: const Color(0xFFD08770),
+          //backgroundColor: const Color(0xFF81A1C1)),
+        )));
+  }
+}
